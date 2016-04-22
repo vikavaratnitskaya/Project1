cb6e916b63174fdd63203a3f3c8f39ae18dcbea6
caveywoolpert <dmitri.kruchek@gmail.com>
2016-04-22 18:20:09 +0300

    Added files via upload

--- utils.py
+++ utils.py
@@ -9,22 +9,27 @@ def ssh_available(host):
     command = "nmap %s" % host
     process = subprocess.check_output(command, shell = True)
     if "ssh" in process:
-        print "ssh is running on", host
         return True
     else:
         print "ssh is not running on", host
         return False   
 
 def host_is_pingable(host):
-    command = "ping -c 3 %s" % host
+    command = "ping -c 2 %s" % host
     if subprocess.call(command, shell=True, 
 	    stdout = open("/dev/null", "w"),
         stderr = subprocess.STDOUT) == 0:
-        print host, "is UP"
         return True
     else:
         print host, "is DOWN"
         return  False
+
+def util_finder(util, ssh):
+    util_dest = ""
+    stdin, stdout, stderr = ssh.exec_command("which %s" % util)
+    for line in stdout:
+        util_dest = line.rstrip()
+    return util_dest
 
 
 
